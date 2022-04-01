2877827530b81471e1babe3c2e11a2f4d25b8806
Brandon Olah <brandon_olah@yahoo.com>
2022-04-01 14:20:14 -0400

    Added Progress Bar For Square Count. Updated Square Count Logic. Published.

---
+++ Forms/BackgroundWorker.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Forms;
+
+namespace L1FEOutdoors.Forms;
+
+public partial class BackgroundWorker : Form
+{
+    public BackgroundWorker()
+    {
+        InitializeComponent();
+    }
+}
